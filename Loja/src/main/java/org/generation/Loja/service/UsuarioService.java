8c6f8867864ce6a311d2e6dad693268d46cbb589
oGraebin <felipegraebin@hotmail.com>
2021-10-07 17:41:21 -0300

    Implementação das camadas de segurança

---
+++ Loja/src/main/java/org/generation/Loja/service/UsuarioService.java
@@ -0,0 +1,52 @@
+package org.generation.Loja.service;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+
+import org.apache.commons.codec.binary.Base64;
+import org.generation.Loja.model.Usuario;
+import org.generation.Loja.model.UsuarioLogin;
+import org.generation.Loja.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository repository;
+
+	public Usuario CadastrarUsuario(Usuario usuario) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+		String senhaEncoder = encoder.encode(usuario.getSenha());
+		usuario.setSenha(senhaEncoder);
+
+		return repository.save(usuario);
+	}
+
+	public Optional<UsuarioLogin> Logar(Optional<UsuarioLogin> user) {
+
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		Optional<Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
+
+		if (usuario.isPresent()) {
+			if (encoder.matches(user.get().getSenha(), usuario.get().getSenha())) {
+
+				String auth = user.get().getUsuario() + ":" + user.get().getSenha();
+				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+				String authHeader = "Basic " + new String(encodedAuth);
+
+				user.get().setToken(authHeader);				
+				user.get().setNome(usuario.get().getNome());
+				user.get().setSenha(usuario.get().getSenha());
+
+				return user;
+
+			}
+		}
+		return null;
+	}
+
+}
